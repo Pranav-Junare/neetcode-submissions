351fae3ded3b587a304b1307abcaac197451eaef
Pranav Junare <pranavjunare7201@gmail.com>
2026-09-26 12:52:27 +0530

    Time: 3 ms (77.51%) | Memory: 43.2 MB (21.78%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -2,31 +2,12 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res= new ArrayList<>();
         
-        int l=0, r=0;
-        while(r<nums.length){
-
-            if(r==nums.length-1){
-                if(nums[l]==nums[r]) res.add(Integer.toString(nums[r]));
-                else{
-                    res.add(nums[l]+"->"+nums[r]);
-                }
-                break;
-            }
-
-            // Checks to move pointer 'r', the check may cuz an out of bound error but lets see
-            if(nums[r]==nums[r+1]-1){
-                r++;
-            }
-
-            // If not same how to add
-            else{
-                if(nums[l]==nums[r]) res.add(Integer.toString(nums[r]));
-                
-                else{
-                    res.add(nums[l]+"->"+nums[r]);
-                }
-                r++;
-                l=r;
+        int start=0;
+        for(int i=0;i<nums.length;i++){
+            if(i==nums.length-1 || nums[i+1]!=nums[i]+1){
+                if(start==i)res.add(String.valueOf(nums[i]));
+                else res.add(nums[start]+"->"+nums[i]);
+                start=i+1;
             }
         }
         return res;
